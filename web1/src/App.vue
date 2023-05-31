2d94342f90ee563c2f0305ade99b77614e401b07
hxf <666451@qq.com>
2023-05-31 14:26:21 +0800

    创建后端

---
+++ web1/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <NavBar/>
+  <router-view/>
+</template>
+<script>
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import NavBar from './components/NavBar';
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
+<style>
+
+
+</style>
